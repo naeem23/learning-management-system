007b48f2986dae5e7b8d36a52bbad13a9cd59883
s.naeem <siddique.naeem@dynamiconsulting.co.uk>
2024-05-24 09:11:13 +0600

    show courses on search page

--- actions/get-course.js
+++ actions/get-course.js
@@ -1,6 +1,7 @@
 import { auth } from '@clerk/nextjs';
 
 import prismadb from '@/lib/prismadb';
+import { getProgress } from './get-progress';
 
 export const getCourse = async (id) => {
     const { userId } = auth();
@@ -26,3 +27,60 @@ export const getCourse = async (id) => {
 
     return course;
 };
+
+
+export const getCourses = async ({userId, title, categoryId}) => {
+    try {
+        const courses = await prismadb.course.findMany({
+            where: {
+                isPublished: true,
+                title: {
+                    contains: title,
+                },
+                categoryId
+            },
+            include: {
+                category: true,
+                chapters: {
+                    where: {
+                        isPublished: true
+                    },
+                    select: {
+                        id: true
+                    }
+                },
+                purchases: {
+                    where: {
+                        userId
+                    }
+                }
+            },
+            orderBy: {
+                createdAt: 'desc'
+            }
+        });
+        
+        const coursesWithProgress = await Promise.all(
+            courses.map(async course => {
+                if (course.purchases.length === 0) {
+                    return {
+                        ...course,
+                        progress: null
+                    }
+                }
+
+                const progressPercentage = await getProgress(userId, course.id);
+
+                return {
+                    ...course,
+                    progress: progressPercentage
+                }
+            })
+        );
+
+        return coursesWithProgress;
+    } catch (error) {
+        console.log("[GET_COURSES]", error);
+        return []
+    }
+}
--- app/(dashboard)/(routes)/search/page.jsx
+++ app/(dashboard)/(routes)/search/page.jsx
@@ -1,22 +1,41 @@
+import { auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+
+import { getCourses } from '@/actions/get-course'
 import Categories from '@/components/categories/categories'
 import { SearchInput } from '@/components/search-input'
 import prismadb from '@/lib/prismadb'
+import { CoursesList } from '@/components/courses/courses-list'
 
-const SearchPage = async () => {
+const SearchPage = async ({ searchParams }) => {
+    const {userId} = auth()
+
+    if (!userId) {
+        return redirect("/")
+    }
+
     const categories = await prismadb.category.findMany({
         orderBy: {
             name: "asc"
         }
     })
+
+    const courses = await getCourses({
+        userId,
+        ...searchParams
+    })
+
   return (
     <>
         <div className='px-6 pt-6 block md:hidden md:mb-0'>
             <SearchInput />
         </div>
-        <div className='p-6'>
+        <div className='p-6 space-y-4'>
             <Categories
                 items={categories}
             />
+
+            <CoursesList items={courses} />
         </div>
     </>
   )
